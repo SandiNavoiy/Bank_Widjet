47b97cb63d024f1dbb535c38ee7089868a3639ff
Sandi_Navoiy <santes.au@gmail.com>
2023-03-12 01:31:17 +0300

    Снапшот на 12.03.2023

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ def load_file():
         return data_new
 
 data = load_file()
+print(data[1])
+
+data.sort(key=lambda x: x.get('id', 0))
+data.pop(0) #чистим первый элемент , ибо он пустой
+print(data)
+data.sort(key=lambda x: x['date'],reverse=True)
 print(data)
 
-data.sort(key=lambda x: x['date'])
-print(data)
+print("Последние 5 операций:")
 
-#for i in load_file():
-#    print(i['date'])
-#    i['date'] = datetime.strptime(i['date'], '%Y-%m-%dT%H:%M:%S.%f')
-#    print(i)
+for i in range(6):
+     transaction = data[i]
+     if "CANCELED" in transaction["state"]:
+         continue
+     else:
+         date = transaction["date"]
+         id = transaction["id"]
+         state = transaction["state"]
+
+         print(f'{date} {id} {state}')
+
 
