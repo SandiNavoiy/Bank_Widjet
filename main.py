ebffa1fa0ecf15099930cde913a44ce91c44766f
Sandi_Navoiy <santes.au@gmail.com>
2023-03-12 15:53:01 +0300

    Реализовано часть функционала по сокрытию номера получателя (или карты)

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import json
 from datetime import datetime
+
+
 def load_file(file_of_json):
     """ функция преобразовывает файл json  в формат Python"""
-    new_lines = []
     with open(file_of_json, 'r', encoding='utf-8') as f:
         data_new = json.loads(f.read())
 
         return data_new
 
+
 def datatime(text):
     """функция преодразования строки к виду число-месяц-год"""
     datetime_object = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%f')
     return f"{datetime_object.day}.{datetime_object.month}.{datetime_object.year}"
+
 
 def sorting_from_empty(list_from_file):
     """ удаление пустых значений и не выполненых операций"""
@@ -23,6 +26,7 @@ def sorting_from_empty(list_from_file):
 
     return list_from_file_new
 
+
 def sorting_from_data(list_from_file):
     """сортировка по дате"""
     list_from_file.sort(key=lambda x: x['date'], reverse=True)
@@ -31,13 +35,31 @@ def sorting_from_data(list_from_file):
 data_transaction = sorting_from_data(sorting_from_empty(load_file("operations.json")))
 
 
+def mask_account_number(account_number):
+    """функция маскировки номера"""
+    account_number_new = account_number["to"]
+    if "Счет" in account_number_new:
+        return account_number_new[0:len(account_number_new) - 20] + '**' + account_number_new[-4:]
+    else:
+        return account_number_new[0:len(account_number_new) - 16] + '**' + account_number_new[-4:]
+
+
+def input_to(account_to):
+    """функция вывода с какой карты или счета идет перевод"""
+    if "from" in account_to:
+        return account_to['from']
+    else:
+        return "вклад открыт"
+
+
 print("Последние 5  проведенных операций по Вашей карте:")
+print("-------------")
+
 
 for i in range(5):
      transaction = data_transaction[i]
-
      print(f'{datatime(transaction["date"])} {transaction["description"]} ')
-     print(f'{transaction["to"]} -> {transaction["to"]}')
+     print(f'{input_to(transaction)} -> {mask_account_number(transaction)}')
      print(f'{transaction["operationAmount"]["amount"]} {transaction["operationAmount"]["currency"]["name"]} ')
      print("-------------")
 
