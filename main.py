058a9d5afdddd87c0680c0f96840bf9af06930cd
Sandi_Navoiy <santes.au@gmail.com>
2023-03-12 11:05:46 +0300

    Преобразование сортировки в отдельные функции

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import json
 from datetime import datetime
-def load_file():
+def load_file(file_of_json):
     """ функция преобразовывает файл json  в формат Python"""
     new_lines = []
-    with open("operations.json", 'r', encoding='utf-8') as f:
+    with open(file_of_json, 'r', encoding='utf-8') as f:
         data_new = json.loads(f.read())
 
         return data_new
 
-data = load_file()
-print(data[1])
+def datatime(text):
+    """функция преодразования строки к виду число-месяц-год"""
+    datetime_object = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%f')
+    return f"{datetime_object.day}.{datetime_object.month}.{datetime_object.year}"
 
-data.sort(key=lambda x: x.get('id', 0))
-data.pop(0) #чистим первый элемент , ибо он пустой
-print(data)
-data.sort(key=lambda x: x['date'],reverse=True)
-print(data)
+def sorting_from_empty(list_from_file):
+    """сортировка по id и удаление пустого значения"""
+    list_from_file.sort(key=lambda x: x.get('id', 0))
+    list_from_file.pop(0)
+    return list_from_file
 
-print("Последние 5 операций:")
+def sorting_from_data(list_from_file):
+    """сортировка по дате"""
+    list_from_file.sort(key=lambda x: x['date'], reverse=True)
+    return list_from_file
+
+data = sorting_from_data(sorting_from_empty(load_file("operations.json")))
+
+
+print("Последние 5  проведенных операций по Вашей карте:")
 
 for i in range(6):
      transaction = data[i]
@@ -28,7 +38,11 @@ for i in range(6):
          id = transaction["id"]
          state = transaction["state"]
 
-         print(f'{date} {id} {state}')
-         print()
+         print(f'{datatime(date)} {id} {state}')
 
+#8.12.2019 863064926 EXECUTED
+#7.12.2019 114832369 EXECUTED
+#19.11.2019 154927927 EXECUTED
+#13.11.2019 482520625 EXECUTED
+#5.11.2019 801684332 EXECUTED
 
