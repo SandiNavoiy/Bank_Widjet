96a8f73fae191e53f5aa5b461544e9ad66f949e8
Sandi_Navoiy <santes.au@gmail.com>
2023-03-17 22:36:15 +0300

    Начало создания тестов

--- tests/test_finc.py
+++ tests/test_finc.py
@@ -2,4 +2,5 @@ from scr.finc import mask_account_number
 
 
 def test_mask_account_number():
-    assert mask_account_number({"to": "Счет 64686473678894779589"}) == "/lihlijli"
+    assert mask_account_number({"to": "Счет 64686473678894779589"}) == "Счет **9589"
+    assert mask_account_number({"to": "Visa Platinum 8990922113665229"}) == "Visa Platinum 8990 09** **** 5229"
