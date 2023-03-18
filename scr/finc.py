ced7d634da387b831d026b384d1d05787a87befd
Sandi_Navoiy <santes.au@gmail.com>
2023-03-18 13:29:54 +0300

    Покрытие 91 процентов блока функций + перенос их их main

--- scr/finc.py
+++ scr/finc.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 def mask_account_number(account_number):
     """функция маскировки номера"""
     new = account_number["to"]
@@ -6,3 +9,43 @@ def mask_account_number(account_number):
         return new[0:len_s - 20] + '**' + new[-4:]
     else:
         return new[0:len_s - 12] + ' ' + new[len_s - 13:len_s - 11] + '** **** ' + new[-4:]
+
+def input_to(account_to):
+    """функция вывода с какой карты или счета идет перевод"""
+    if "from" in account_to:
+        new = account_to['from']
+        len_s = len(new)
+        if "Счет" in new:
+            return new[0:len_s - 20] + '**' + new[-4:]
+        else:
+            return new[0:len_s - 12] + ' ' + new[len_s - 13:len_s - 11] + '** **** ' + new[-4:]
+    else:
+        return "Выполнен перевод на счет вклада"
+
+def datatime(text):
+    """функция преодразования строки к виду число-месяц-год"""
+    datetime_object = datetime.strptime(text, '%Y-%m-%dT%H:%M:%S.%f')
+    return f"{datetime_object.day}.{datetime_object.month}.{datetime_object.year}"
+
+
+def sorting_from_empty(list_from_file):
+    """ удаление пустых значений и не выполненых операций"""
+    list_from_file_new = []
+    for i in list_from_file:
+
+        if i.get('state') == 'EXECUTED':
+            list_from_file_new.append(i)
+
+    return list_from_file_new
+
+def load_file(file_of_json):
+    """ функция преобразовывает файл json  в формат Python"""
+    with open(file_of_json, 'r', encoding='utf-8') as f:
+        data_new = json.loads(f.read())
+
+        return data_new
+
+def sorting_from_data(list_from_file):
+    """сортировка по дате"""
+    list_from_file.sort(key=lambda x: x['date'], reverse=True)
+    return list_from_file
--- tests/test_finc.py
+++ tests/test_finc.py
@@ -1,6 +1,26 @@
 from scr.finc import mask_account_number
-
+from scr.finc import input_to
+from scr.finc import datatime
+from scr.finc import sorting_from_empty
+from scr.finc import sorting_from_data
 
 def test_mask_account_number():
     assert mask_account_number({"to": "Счет 64686473678894779589"}) == "Счет **9589"
     assert mask_account_number({"to": "Visa Platinum 8990922113665229"}) == "Visa Platinum 8990 09** **** 5229"
+
+def test_input_to():
+    assert input_to({"from": "Visa Classic 6831982476737658"}) == "Visa Classic 6831 19** **** 7658"
+    assert input_to({"from": "Счет 48894435694657014368"}) == "Счет **4368"
+    assert input_to({}) == "Выполнен перевод на счет вклада"
+
+def test_datatime():
+    assert datatime("2019-03-23T01:09:46.296404") == "23.3.2019"
+
+
+def test_sorting_from_empty():
+    assert sorting_from_empty([]) == []
+    assert sorting_from_empty([{"state": "EXECUTED"}]) == [{"state": "EXECUTED"}]
+
+def test_sorting_from_data():
+    assert sorting_from_data([{"date": "2019-08-26T10:50:58.294041"},{"date": "2019-07-03T18:35:29.512364"} ]) == [{'date': '2019-08-26T10:50:58.294041'}, {'date': '2019-07-03T18:35:29.512364'}]
+    assert sorting_from_data([{"date": "2019-07-26T10:50:58.294041"},{"date": "2019-08-03T18:35:29.512364"}]) == [{'date': '2019-08-03T18:35:29.512364'}, {'date': '2019-07-26T10:50:58.294041'}]
